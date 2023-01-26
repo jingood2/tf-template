8a384a5b1544e0eb5094384dead3255b19bdd809
james <james@pui-Macmini.local>
2023-01-27 05:53:34 +0900

    update

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@ resource "aws_instance" "my_vm" {
   ami           = var.ami //Ubuntu AMI
   instance_type = var.instance_type
 
+  monitoring           = true
+  ebs_optimized        = true
+  iam_instance_profile = "my-vm"
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+
   tags = {
     Name = var.name_tag,
   }
